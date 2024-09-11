6fb91e77fae364c656a2466119898e4d66d37b9c
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-09-11 07:28:19 +0500

    masala

---
+++ app.js
@@ -0,0 +1,81 @@
+{
+    // console.log("Masala-1")
+    // function multiply(a, b){
+    //     return a * b
+    //   }
+    //   console.log(multiply(5,10))
+}
+{
+    // console.log("Masala-2")
+    // const evenOrOdd = (number) => number % 2 ? "Odd" : 'Even'; 
+    // console.log(evenOrOdd(5))
+}
+{
+    // console.log("Masala-3")
+    // function numberToString(num) {
+    //     return num.toString();
+    // }
+    // console.log(numberToString(5))
+}
+{
+    // console.log("Masala-4")
+    // const opposite = n => -n;
+    // console.log(opposite(5))
+}
+{
+    // console.log("Masala-5")
+    // function solution(str){
+    //     return str.split('').reverse().join('');  
+    // }
+    // console.log(solution("Ibrohim"))
+}
+{
+    // console.log("Masala-6")
+    // function stringToArray(string){
+    //     const wordsArray = [...string.match(/\S+/g)];
+    //     return wordsArray
+    // }
+    // console.log(stringToArray("Salom Dunyo"))
+}
+{
+    // console.log("Masala-7")
+    // function makeNegative(num){
+    //     if(num>=0){
+    //         return -num
+    //     }if(num<0){
+    //         return num
+    //     }
+    // }
+    // console.log(makeNegative(-5))
+}
+{
+    // console.log("Masala-8")
+    // function booleanToString(b){
+    //     return String(b);
+    // }
+    // console.log(booleanToString(false))
+
+}
+{
+    // console.log("Masala-9")
+    // function boolToWord( bool ){
+    //     if(bool==true){
+    //       return "Yes"
+    //     }else{
+    //       return "No"
+    //     }
+    // }
+    // console.log(boolToWord(true))
+}
+{
+    // console.log("Masala-10")
+    // const stringToNumber = str => Number(str)
+    // console.log(stringToNumber("5"))
+}
+
+{
+    // console.log("Masala-11")
+    // function greet(s){
+    //     return "hello world!"
+    // }
+}
